45d3c29d4a2b2ebf8b5f09ee90ace3980cb2fe2c
aashikohad <kohaday@rknec.edu>
2026-03-06 17:07:21 +0530

    done

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 # Database model
@@ -16,38 +18,43 @@ class URL(db.Model):
 # Generate short code
 def generate_code():
     characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(6))
+    while True:
+        code = ''.join(random.choice(characters) for _ in range(6))
+        existing = URL.query.filter_by(short_code=code).first()
+        if not existing:
+            return code
 
 # Home page
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     short_url = None
-    
+
     if request.method == 'POST':
         long_url = request.form['url']
-        
+
         code = generate_code()
-        
+
         new_url = URL(long_url=long_url, short_code=code)
         db.session.add(new_url)
         db.session.commit()
-        
+
         short_url = request.host_url + code
-    
+
     return render_template('index.html', short_url=short_url)
 
 # Redirect route
 @app.route('/<code>')
 def redirect_url(code):
     url = URL.query.filter_by(short_code=code).first()
-    
+
     if url:
         return redirect(url.long_url)
-    
+
     return "URL not found"
 
+# Run application
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        
-   app.run(host="0.0.0.0", port=10000)
+
+    app.run(host="0.0.0.0", port=10000, debug=True)
